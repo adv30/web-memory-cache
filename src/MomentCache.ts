ac36fc48438803117f4867902fb496e9c69df606
adv30 <adv30@126.com>
2019-08-28 14:38:49 +0800

    add MomentCache

--- src/MomentCache.ts
+++ src/MomentCache.ts
@@ -1,11 +1,35 @@
-import Pool from './Pool'
-
 /**
  * 瞬态缓存
  * 1. 当下次取值的key不是上一次的key时,清除掉上一次的key
  * 2. 当超时一段时间(50ms)时,清除掉所有的key
  * 3. 具体的取值对象是保存对象还是通过资源池去取(一个方法?)需要考虑
  */
-class Momentcache {}
+class MomentCache {
+    private beforeElement: any = null
+    private beforeKey: string = ''
+    private beforeVal: any = undefined
+    private beforeTime: number = 0
+    private loseTime: number = 50
 
-export default Momentcache
+    public get(el: any, key: string) {
+        if (
+            this.beforeElement === null ||
+            this.beforeKey !== key ||
+            el.id !== this.beforeElement.id ||
+            new Date().getMilliseconds() - this.beforeTime > this.loseTime
+        ) {
+            this.save(el, key)
+        }
+        this.beforeTime = new Date().getMilliseconds()
+        return this.beforeVal
+    }
+
+    private save(el: any, key: string) {
+        this.beforeElement = el
+        this.beforeKey = key
+        this.beforeVal = el[key]
+        this.beforeTime = new Date().getMilliseconds()
+    }
+}
+
+export default MomentCache
---
+++ src/__tests__/MomentCache.testMGet.ts
@@ -0,0 +1,33 @@
+import MomentCache from '../MomentCache'
+
+class TestObj {
+    public id = 'aabbb'
+    private num = 0
+
+    public get myNum() {
+        return this.num++
+    }
+}
+const objA = new TestObj()
+const cache = new MomentCache()
+
+const objB = new TestObj()
+objB.id = 'xxxxyyyy'
+
+console.log(objA.myNum)
+console.log('objA', cache.get(objA, 'myNum'))
+console.log('objA', cache.get(objA, 'myNum'))
+console.log('objA', cache.get(objA, 'myNum'))
+console.log('objB', cache.get(objB, 'myNum'))
+console.log('objB', cache.get(objB, 'myNum'))
+console.log('objB', cache.get(objB, 'myNum'))
+console.log('objB', cache.get(objB, 'myNum'))
+console.log('objB', cache.get(objB, 'myNum'))
+console.log('objA', cache.get(objA, 'myNum'))
+console.log('objA', cache.get(objA, 'myNum'))
+console.log('objA', cache.get(objA, 'myNum'))
+console.log('objA', cache.get(objA, 'myNum'))
+
+test('MomentCache', () => {
+    expect(null).toBe(null)
+})
